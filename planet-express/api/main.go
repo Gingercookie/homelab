fa8174420ed3b91c6f8a7628c8d2dfd36bbdbb20
Will Graham <wgraham800@gmail.com>
2025-12-31 11:21:39 -0700

    feat: api: add prometheus metrics and logging

--- planet-express/api/main.go
+++ planet-express/api/main.go
@@ -5,8 +5,11 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
@@ -27,12 +30,14 @@ func handleNewDelivery(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Println("[INFO] Got request for new delivery")
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Unable to read request", http.StatusBadRequest)
 		return
 	}
 
+	fmt.Printf("[INFO] Dispatching request to delivery-service at %s\n", deliveryServiceURL)
 	resp, err := http.Post(fmt.Sprintf("%s/deliveries", deliveryServiceURL), "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		http.Error(w, "Error contacting DeliveryService: "+err.Error(), http.StatusServiceUnavailable)
@@ -49,9 +54,26 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/deliveries", handleNewDelivery)
-	http.HandleFunc("/health", healthCheck)
+	apiMux := http.NewServeMux()
+	apiMux.HandleFunc("/deliveries", handleNewDelivery)
+	apiMux.HandleFunc("/health", healthCheck)
 
-	fmt.Println("PlanetExpressAPI running on :8080")
-	http.ListenAndServe(":8080", nil)
+	metricsMux := http.NewServeMux()
+	metricsMux.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		fmt.Println("Prometheus metrics endpoint running on :2112")
+		err := http.ListenAndServe(":2112", metricsMux)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	go func() {
+		fmt.Println("PlanetExpressAPI running on :8080")
+		err := http.ListenAndServe(":8080", apiMux)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
 }
